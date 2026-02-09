0addb03afd7b10412b842b15da8a2440092221cb
u-a-b <uamayabanegas26@aoiths.org>
2026-02-09 14:05:00 -0500

    progress

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,41 @@
-document.querySelector(".btn-primary").addEventListener("click", function (event) {
+let form = document.querySelector("form");
+let firstName = document.querySelector("#first");
+let lastName = document.querySelector("#last");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let country = document.querySelector("#country");
+let standard = document.querySelector("#standard[name='accountType']:checked");
+let premium = document.querySelector("#premium[name='accountType']:checked");
+let text = document.querySelector("#text");
 
-    const firstName = document.querySelector("");
+form.addEventListener("click", function(){
+form.preventDefault();
+});
+
+document.querySelector(".btn-primary").addEventListener("click", function (event) {
+    let firstNameValue = firstName.value;
+    let lastNameValue = lastName.value;
+    let emailValue =  email.value;
+    let passwordValue = password.value;
+let countryValue = country.value;
+let standardValue = standard.value;
+let premiumValue = premium.value;
+let textValue = text.value;
+
+const user = {
+first: firstNameValue,
+last: lastNameValue,
+email: emailValue,
+password: passwordValue,
+country: countryValue,
+standard: standardValue,
+premium: premiumValue,
+text: textValue
+};
+
+let jsonString = JSON.stringify(user);
+localStorage.setItem("registeredUser", jsonString);
+
 
 });
+
