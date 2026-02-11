62d183d21a2ff31f8b7b3c84e269e11c0df0e696
u-a-b <144457851+u-a-b@users.noreply.github.com>
2026-02-11 14:01:43 -0500

    progress 2

--- src/js/script.js
+++ src/js/script.js
@@ -8,18 +8,19 @@ let standard = document.querySelector("#standard[name='accountType']:checked");
 let premium = document.querySelector("#premium[name='accountType']:checked");
 let text = document.querySelector("#text");
 
-form.addEventListener("click", function(){
-form.preventDefault();
+form.addEventListener("click", function(event){
+event.preventDefault();
 });
 
 document.querySelector(".btn-primary").addEventListener("click", function (event) {
+    event.preventDefault();
     let firstNameValue = firstName.value;
     let lastNameValue = lastName.value;
     let emailValue =  email.value;
     let passwordValue = password.value;
 let countryValue = country.value;
-let standardValue = standard.value;
-let premiumValue = premium.value;
+let standardValue = standard;
+let premiumValue = premium;
 let textValue = text.value;
 
 const user = {
@@ -36,6 +37,25 @@ text: textValue
 let jsonString = JSON.stringify(user);
 localStorage.setItem("registeredUser", jsonString);
 
+let savedFirst = document.querySelector("#savedFirstName");
+let savedLast = document.querySelector("#savedLastName");
+let savedEmail = document.querySelector("#savedEmail");
+let savedCountry = document.querySelector("#savedCountry");
+let savedAccountType = document.querySelector("#savedAccountType");
+ let savedAbout = document.querySelector("#savedAbout");
 
+ savedFirst.textContent = user.first;
+ savedLast.textContent = user.last;
+savedEmail.textContent = user.email;
+savedCountry.textContent = user.country;
+
+if(standardValue === true){
+    savedAccountType.textContent = user.standard;
+}
+else if(premiumValue === true){
+    savedAccountType.textContent = user.premium;
+}
+
+savedAbout.textContent = user.text;
 });
 
